77e95f975550c2f872a97abfe76b1c6f4965aee6
your_username <kajantek@gmail.com>
2020-12-29 22:32:45 +0100

    array useState

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
+
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let title = 'random title';
+  const handleClick = () => {
+    title = 'hello people';
+    console.log(title);
+  };
+
+return (
+    <React.Fragment>
+      <h2>
+        {title}
+        <button type='button' className='btn' onClick={handleClick}>cambia il titolo</button>
+      </h2>
+    </React.Fragment>
+  );
+
 };
 
 export default ErrorExample;
--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+
+  const [text, setText] = useState('primo text');
+
+  const handleClick = () => {
+    if (text === 'primo text') {
+      setText('secondo text'); 
+    } else {
+      setText('primo text');
+  } 
+};
+  
+
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button type="button" onClick={handleClick}>Cambia titolo</button>
+    </React.Fragment>
+  )
 };
 
 export default UseStateBasics;
